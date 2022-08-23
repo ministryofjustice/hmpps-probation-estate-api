bfffe9b97e21909067320bbfe43e6e2140e210b9
carlov20 <carlov20@users.noreply.github.com>
2022-08-23 16:24:06 +0100

    Wfp 1325 (#5)
    * WFP-1325 setup db integration
    
    * WFP-1325 upgraded to jdk 18
    
    * WFP-1325 added get teams by pdu endpoint
    
    * WFP-1325 added not found scenario
    
    * WFP-1325 lint fix
    
    * WFP-1325 removed tomcat from app, fixed swagger

--- README.md
+++ README.md
@@ -1,40 +1,33 @@
-# hmpps-template-kotlin
+# hmpps-probation-estate-api
 
-This is a skeleton project from which to create new kotlin projects from.
+[![CircleCI](https://circleci.com/gh/ministryofjustice/hmpps-probation-estate-api/tree/main.svg?style=svg)](https://circleci.com/gh/ministryofjustice/hmpps-probation-estate-api)
+[![API docs](https://img.shields.io/badge/API_docs-view-85EA2D.svg?logo=swagger)](https://hmpps-probation-estate-api-dev.hmpps.service.justice.gov.uk/swagger-ui.html)
 
-# Instructions
+This is the HMPPS Probation Estate API. This is used to serve Probation estate information
 
-If this is a HMPPS project then the project will be created as part of bootstrapping - 
-see https://github.com/ministryofjustice/dps-project-bootstrap.
+## Instructions
 
-## Creating a CloudPlatform namespace
+To run locally use docker to create a Postgres instance with the following command:
 
-When deploying to a new namespace, you may wish to use this template kotlin project namespace as the basis for your new namespace:
+```shell
+docker compose up -d
+```
 
-<https://github.com/ministryofjustice/cloud-platform-environments/tree/main/namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-template-kotlin>
+Then execute the command
+```shell
+./gradlew bootRun
+```
 
-Copy this folder, update all the existing namespace references, and submit a PR to the CloudPlatform team. Further instructions from the CloudPlatform team can be found here: <https://user-guide.cloud-platform.service.justice.gov.uk/#cloud-platform-user-guide>
+## testing
 
-## Renaming from HMPPS Template Kotlin - github Actions
+These tests are defaults from the template, and the app only serves to run the flyway scripts. This allows you to test that the scripts work locally against postgresql in docker
+```shell
+docker compose up -d
+./gradlew clean check
+```
 
-Once the new repository is deployed. Navigate to the repository in github, and select the `Actions` tab.
-Click the link to `Enable Actions on this repository`.
-
-Find the Action workflow named: `rename-project-create-pr` and click `Run workflow`.  This workflow will
-execute the `rename-project.bash` and create Pull Request for you to review.  Review the PR and merge.
-
-Note: ideally this workflow would run automatically however due to a recent change github Actions are not
-enabled by default on newly created repos. There is no way to enable Actions other then to click the button in the UI.
-If this situation changes we will update this project so that the workflow is triggered during the bootstrap project.
-Further reading: <https://github.community/t/workflow-isnt-enabled-in-repos-generated-from-template/136421>
-
-## Manually renaming from HMPPS Template Kotlin
-
-Run the `rename-project.bash` and create a PR.
-
-The `rename-project.bash` script takes a single argument - the name of the project and calculates from it:
-* The main class name (project name converted to pascal case) 
-* The project description (class name with spaces between the words)
-* The main package name (project name with hyphens removed)
-
-It then performs a search and replace and directory renames so the project is ready to be used.
+## Code style & formatting
+```shell
+./gradlew ktlintApplyToIdea addKtlintFormatGitPreCommitHook
+```
+will apply ktlint styles to intellij and also add a pre-commit hook to format all changed kotlin files.
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprobationestateapi/config/HmppsProbationEstateApiExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprobationestateapi/config/HmppsProbationEstateApiExceptionHandler.kt
@@ -9,64 +9,73 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.hmppsprobationestateapi.exception.EntityNotFoundException
 import javax.validation.ValidationException
 
 @RestControllerAdvice
 class HmppsProbationEstateApiExceptionHandler {
   @ExceptionHandler(ValidationException::class)
-  fun handleValidationException(e: Exception): ResponseEntity<ErrorResponse> {
+  fun handleValidationException(e: Exception): Mono<ResponseEntity<ErrorResponse>> {
     log.info("Validation exception: {}", e.message)
-    return ResponseEntity
-      .status(BAD_REQUEST)
-      .body(
-        ErrorResponse(
-          status = BAD_REQUEST,
-          userMessage = "Validation failure: ${e.message}",
-          developerMessage = e.message
+    return Mono.just(
+      ResponseEntity
+        .status(BAD_REQUEST)
+        .body(
+          ErrorResponse(
+            status = BAD_REQUEST,
+            userMessage = "Validation failure: ${e.message}",
+            developerMessage = e.message
+          )
         )
-      )
+    )
   }
 
   @ExceptionHandler(EntityNotFoundException::class)
-  fun handleEntityNotFoundException(e: Exception): ResponseEntity<ErrorResponse> {
+  fun handleEntityNotFoundException(e: Exception): Mono<ResponseEntity<ErrorResponse>> {
     log.info("Entity not found exception: {}", e.message)
-    return ResponseEntity
-      .status(NOT_FOUND)
-      .body(
-        ErrorResponse(
-          status = NOT_FOUND,
-          userMessage = "Entity not found failure: ${e.message}",
-          developerMessage = e.message
+    return Mono.just(
+      ResponseEntity
+        .status(NOT_FOUND)
+        .body(
+          ErrorResponse(
+            status = NOT_FOUND,
+            userMessage = "Entity not found failure: ${e.message}",
+            developerMessage = e.message
+          )
         )
-      )
+    )
   }
 
   @ExceptionHandler(AccessDeniedException::class)
-  fun handleAccessDeniedException(e: Exception): ResponseEntity<ErrorResponse> {
-    return ResponseEntity
-      .status(HttpStatus.FORBIDDEN)
-      .body(
-        ErrorResponse(
-          status = HttpStatus.FORBIDDEN,
-          userMessage = "Access is denied",
-          developerMessage = e.message
+  fun handleAccessDeniedException(e: Exception): Mono<ResponseEntity<ErrorResponse>> {
+    return Mono.just(
+      ResponseEntity
+        .status(HttpStatus.FORBIDDEN)
+        .body(
+          ErrorResponse(
+            status = HttpStatus.FORBIDDEN,
+            userMessage = "Access is denied",
+            developerMessage = e.message
+          )
         )
-      )
+    )
   }
 
   @ExceptionHandler(java.lang.Exception::class)
-  fun handleException(e: java.lang.Exception): ResponseEntity<ErrorResponse?>? {
+  fun handleException(e: java.lang.Exception): Mono<ResponseEntity<ErrorResponse>> {
     log.error("Unexpected exception", e)
-    return ResponseEntity
-      .status(INTERNAL_SERVER_ERROR)
-      .body(
-        ErrorResponse(
-          status = INTERNAL_SERVER_ERROR,
-          userMessage = "Unexpected error: ${e.message}",
-          developerMessage = e.message
+    return Mono.just(
+      ResponseEntity
+        .status(INTERNAL_SERVER_ERROR)
+        .body(
+          ErrorResponse(
+            status = INTERNAL_SERVER_ERROR,
+            userMessage = "Unexpected error: ${e.message}",
+            developerMessage = e.message
+          )
         )
-      )
+    )
   }
 
   companion object {
