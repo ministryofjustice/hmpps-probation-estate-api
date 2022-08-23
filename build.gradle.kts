c95445557cacca427f1a662607c7283a2a11f146
carlov20 <carlov20@users.noreply.github.com>
2022-08-23 10:45:55 +0100

    WFP-1325 setup db integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,16 @@ configurations {
 val springDocVersion by extra("1.6.10")
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   implementation("org.springdoc:springdoc-openapi-webflux-ui:$springDocVersion")
   implementation("org.springdoc:springdoc-openapi-kotlin:$springDocVersion")
   implementation("org.springdoc:springdoc-openapi-data-rest:$springDocVersion")
+
+  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("io.r2dbc:r2dbc-postgresql:0.8.12.RELEASE")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mock-server:mockserver-netty:5.13.2")
@@ -23,9 +27,6 @@ dependencies {
 
 java {
   toolchain.languageVersion.set(JavaLanguageVersion.of(18))
-}
-repositories {
-  mavenCentral()
 }
 
 tasks {
