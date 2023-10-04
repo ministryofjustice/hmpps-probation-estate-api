0184e01053c48df3fc29d4e0d6bfd58b9db842bd
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2023-10-04 14:25:05 +0100

    :sparkles: WFP-2151: added LDU and updated required teams to use it (#161)

---
+++ src/main/resources/db/migration/V1_21__UPDATE_KENSINGTON_LDU.sql
@@ -0,0 +1,11 @@
+UPDATE team
+    SET ldu_code = 'LDNNKCW'
+WHERE code = 'N07KEN';
+
+UPDATE team
+    SET ldu_code = 'LDNNKCW'
+WHERE code = 'N07562';
+
+UPDATE team
+    SET ldu_code = 'LDNNKCW'
+WHERE code = 'N07001';
