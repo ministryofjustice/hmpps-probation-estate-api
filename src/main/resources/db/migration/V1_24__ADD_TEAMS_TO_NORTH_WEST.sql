8b04645126b596bbdf43ff27d74d9d4a83edb562
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2023-10-11 15:20:36 +0100

    :card_file_box: WFP-2192: remove whitespace

--- src/main/resources/db/migration/V1_24__ADD_TEAMS_TO_NORTH_WEST.sql
+++ src/main/resources/db/migration/V1_24__ADD_TEAMS_TO_NORTH_WEST.sql
@@ -1,3 +1,2 @@
 INSERT INTO team (code, name, ldu_code) VALUES ('N51SL6', 'Liverpool South 6', 'N51SOU');
 INSERT INTO team (code, name, ldu_code) VALUES ('N51MAI', 'Liverpool South 7', 'N51SOU');
-
