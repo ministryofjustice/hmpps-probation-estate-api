b818d13508bab962c8ec8d53d5c2a3295e3d6e77
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-12-01 10:35:13 +0000

    add additional Newport, Nottingham, and Barking teams

---
+++ src/main/resources/db/migration/V1_34__ADD_NEWPORT_TEAM_4.sql
@@ -0,0 +1,1 @@
+INSERT INTO team (code, name, ldu_code) VALUES ('N03NP4', 'Newport Team 4', 'WPTGWT');
---
+++ src/main/resources/db/migration/V1_35__ADD_NOTTINGHAM_PRERELEASE_TEAM.sql
@@ -0,0 +1,1 @@
+INSERT INTO team (code, name, ldu_code) VALUES ('N53NPR', 'Nottingham Pre-release Team', 'N53NOTL');
