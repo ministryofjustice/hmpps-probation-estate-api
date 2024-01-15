14dc9d048a06aa5f53a273b07381e3de5d97b704
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2024-01-15 15:53:54 +0000

    :card_file_box: WFP-2322: remove North East team already added

--- src/main/resources/db/migration/V1_44__ADD_NEW_NORTH_EAST_TEAMS.sql
+++ src/main/resources/db/migration/V1_44__ADD_NEW_NORTH_EAST_TEAMS.sql
@@ -1,2 +1,1 @@
-INSERT INTO team (code, name, ldu_code) VALUES ('N54SIO', 'South of Tyne IOM', 'N54NTS');
 INSERT INTO team (code, name, ldu_code) VALUES ('N54SU4', 'BCCF Pending Allocations Central', 'N54SDL');
