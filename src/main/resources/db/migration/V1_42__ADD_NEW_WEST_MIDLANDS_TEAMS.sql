bedbdd74142fdc4042500ecdd782521c5557c92c
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2024-01-16 08:46:32 +0000

    :card_file_box: WFP-2322: amend west midlands team name

--- src/main/resources/db/migration/V1_42__ADD_NEW_WEST_MIDLANDS_TEAMS.sql
+++ src/main/resources/db/migration/V1_42__ADD_NEW_WEST_MIDLANDS_TEAMS.sql
@@ -4,7 +4,7 @@ INSERT INTO team (code, name, ldu_code) VALUES ('N52AL4', 'BCCF Pending Allocati
 INSERT INTO team (code, name, ldu_code) VALUES ('N52AL3', 'BCCF Pending Allocations North', 'N52BIRL');
 INSERT INTO team (code, name, ldu_code) VALUES ('N52AL1', 'BCCF Pending Allocations South', 'N52BIRL');
 INSERT INTO team (code, name, ldu_code) VALUES ('N52BlO', 'Birmingham Integrated Offender Management', 'N52BIRL');
-INSERT INTO team (code, name, ldu_code) VALUES ('N52BlS', 'Birmingham Integrated Offender Management', 'N52BIRL');
+INSERT INTO team (code, name, ldu_code) VALUES ('N52BlS', 'Intensive Supervision Team Pilot', 'N52BIRL');
 INSERT INTO team (code, name, ldu_code) VALUES ('N52YAC', 'Young Adults Central Birmingham', 'N52BIRL');
 INSERT INTO team (code, name, ldu_code) VALUES ('N52YAE', 'Young Adults East Birmingham', 'N52BIRL');
 INSERT INTO team (code, name, ldu_code) VALUES ('N52YAN', 'Young Adults North Birmingham', 'N52BIRL');
