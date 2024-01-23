6f01a9f566d96aa93cd9fcd01c1250def00a3efa
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2024-01-23 10:27:00 +0000

    :card_file_box: WFP-2330: added new North West, West Midlands and East Midlands teams (#193)

---
+++ src/main/resources/db/migration/V1_47__ADD_NEW_NORTH_WEST_WEST_MIDS_EAST_MIDS_TEAMS.sql
@@ -0,0 +1,3 @@
+INSERT INTO team (code, name, ldu_code) VALUES ('N51BL4', 'Blackburn 4', 'N51BDRV');
+INSERT INTO team (code, name, ldu_code) VALUES ('N53NC7', 'Nottingham City 7', 'N53NOTL');
+INSERT INTO team (code, name, ldu_code) VALUES ('N52CB9', 'Central Birmingham 6', 'N52BLDU');
