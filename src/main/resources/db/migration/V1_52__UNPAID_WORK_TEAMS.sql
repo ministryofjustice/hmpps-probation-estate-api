d8a800bbbe77a74cf8a288afd24b439290c52cfd
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-03-20 11:43:41 +0000

    WFP-2361-adding-unpaid-work-teams (#202)

---
+++ src/main/resources/db/migration/V1_52__UNPAID_WORK_TEAMS.sql
@@ -0,0 +1,53 @@
+/*Greater Manchester N50*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N50UPAC', 'Unpaid Work and Attendance Centres', 'N50');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N50UPW', 'Unpaid Work', 'N50UPAC');
+INSERT INTO team (code, name, ldu_code) VALUES ('N50CBA', 'GM SM CP Bury Rochdale Oldham Tameside', 'N50UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N50CAZ', 'GM SM CP Bolton Wigan Salford', 'N50UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N50CAY', 'GM SM CP Manchester Stockport Trafford', 'N50UPW');
+/*North West N51*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N51UPAC', 'Unpaid Work and Attendance Centres', 'N51');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N51UPW', 'Unpaid Work', 'N51UPAC');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51CAZ', 'Cumbria and Lancashire UPW Hub', 'N51UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51MAQ', 'Merseyside and Cheshire UPW Hub Team 1', 'N51UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51MAN', 'Merseyside and Cheshire UPW Hub Team 2', 'N51UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51MAO', 'Merseyside and Cheshire UPW Hub Team 3', 'N51UPW');
+/*East Midlands Region N53*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N53UPAC', 'Unpaid Work and Attendance Centres', 'N53');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N53UPW', 'Unpaid Work', 'N53UPAC');
+INSERT INTO team (code, name, ldu_code) VALUES ('N53DBH', 'UPW Nottinghamshire', 'N53UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N53DGA', 'UPW Derbyshire', 'N53UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N53DAX', 'UPW Leicestershire', 'N53UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N53LAT', 'UPW Lincolnshire', 'N53UPW');
+/*West Midlands Region N52*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N52USRO', 'UPW SRO', 'N52');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N52USRO', 'UPW SRO', 'N52USRO');
+INSERT INTO team (code, name, ldu_code) VALUES ('N52UCN', 'UPW SRO Central and North', 'N52USRO');
+INSERT INTO team (code, name, ldu_code) VALUES ('N52USE', 'UPW SRO South and East', 'N52USRO');
+/*South Central N59*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N59UPAC', 'Unpaid Work and Attendance Centres', 'N59');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N59UPW', 'Unpaid Work', 'N59UPAC');
+INSERT INTO team (code, name, ldu_code) VALUES ('N59HIJ', 'CP North', 'N59UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N59HII', 'CP South West', 'N59UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N59HIH', 'CP South East', 'N59UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N59TAY', 'Unpaid Work North Hub', 'N59UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N59TAZ', 'Unpaid Work South Hub', 'N59UPW');
+/*London N07*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N07UPAC', 'Unpaid Work and Attendance Centres', 'N07');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N07UPW', 'Unpaid Work', 'N07UPAC');
+INSERT INTO team (code, name, ldu_code) VALUES ('N07LDQ', 'CP North East 1', 'N07UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N07LDH', 'CP North East 2', 'N07UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N07LCA', 'CP South 1', 'N07UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N07LCB', 'CP South 2', 'N07UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N07LEN', 'CP West 1', 'N07UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N07LEO', 'CP West 2', 'N07UPW');
+/*East of England N56*/
+INSERT INTO pdu (code, name, region_code) VALUES ('N56UPAC', 'Unpaid Work and Attendance Centres', 'N56');
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N56UPW', 'Unpaid Work', 'N56UPAC');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56BBD', 'BDS-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56BBC', 'CBS-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56EBD', 'EXN-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56EBF', 'EXS-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56BBE', 'HFS-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56NAN', 'NFK-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56BBF', 'NTH-UPW SA', 'N56UPW');
+INSERT INTO team (code, name, ldu_code) VALUES ('N56NAV', 'SFK-UPW SA', 'N56UPW');
