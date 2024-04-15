576a9ac0d0318a3b2fa8c8e7b6fe5b9547bf7606
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-04-15 12:24:23 +0100

    WFP-2462-Amending team names for SC (#206)

---
+++ src/main/resources/db/migration/V1_56__RENAME_TEAMS_UPW_SOUTH_CENTRAL.sql
@@ -0,0 +1,22 @@
+/* changes for WFP-2462 */
+
+UPDATE team
+SET name = 'UPW SC - Berkshire'
+WHERE code = 'N59TAZ';
+
+UPDATE team
+SET name = 'UPW SC - North Hants'
+WHERE code = 'N59HIJ';
+
+UPDATE team
+SET name = 'UPW SC - Ox & Bucks'
+WHERE code = 'N59TAY';
+
+UPDATE team
+SET name = 'UPW SC - PIOW & East Hants'
+WHERE code = 'N59HIH';
+
+UPDATE team
+SET name = 'UPW SC - SENF'
+WHERE code = 'N59HII';
+
