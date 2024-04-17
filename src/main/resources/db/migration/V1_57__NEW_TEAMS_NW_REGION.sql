51440b3b6685513796bed3bf534de881df0e3298
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-04-17 21:22:21 +0100

    WFP-2464-new nw region teams

---
+++ src/main/resources/db/migration/V1_57__NEW_TEAMS_NW_REGION.sql
@@ -0,0 +1,6 @@
+/* WFP-2464 */
+INSERT INTO team (code, name, ldu_code) VALUES ('N51KC1','KSH Community 1','N51KNOS');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51KC2','KSH Community 2','N51KNOS');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51KR1','KSH Resettlement 1','N51KNOS');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51KR2','KSH Resettlement 2','N51KNOS');
+INSERT INTO team (code, name, ldu_code) VALUES ('N51KIO','KSH IOM','N51KNOS');
