1032c9d0f147060408e9b6d5ca702b96222d78e6
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2024-04-22 14:50:58 +0100

    :card_file_box: WFP-2470: amend ldu code

--- src/main/resources/db/migration/V1_58__ADD_NEW_TEAM_TO_SOUTH-WEST-REGION.sql
+++ src/main/resources/db/migration/V1_58__ADD_NEW_TEAM_TO_SOUTH-WEST-REGION.sql
@@ -1,1 +1,1 @@
-INSERT INTO team (code, name, ldu_code) VALUES ('N58BCP', 'BSG Sent Mgmt – Bristol CP Standalone', 'N58BSG');
+INSERT INTO team (code, name, ldu_code) VALUES ('N58BCP', 'BSG Sent Mgmt – Bristol CP Standalone', 'N58BRL');
