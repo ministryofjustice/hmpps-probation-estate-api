af4ed45066de2562201688ea6f333dbe2f2da688
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-05-09 21:05:01 +0100

    WFP-2469 - onboarding region - wrong PDU's

---
+++ src/main/resources/db/migration/V1_61__KSS_YATH_CORRECTIONS.sql
@@ -0,0 +1,6 @@
+UPDATE ldu SET pdu_code = 'N57SUS' where code = 'N57SUSL';
+UPDATE ldu SET pdu_code = 'N57EKNT' where code = 'N57EKNT';
+UPDATE ldu SET pdu_code = 'N57SUR' where code = 'N57SURL';
+UPDATE ldu SET pdu_code = 'N57UAT' where code = 'N57UAT';
+UPDATE ldu SET pdu_code = 'N57KECL' where code = 'N57KTLD';
+UPDATE ldu SET pdu_code = 'N57WSUS' where code = 'N57WSUS';
