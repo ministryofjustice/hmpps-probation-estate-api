14df39d2ed37bac257ffe14f0bafb8c8e34566f7
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2024-05-20 09:48:48 +0100

    :card_file_box: WFP-2509: add new teams

---
+++ src/main/resources/db/migration/V1_62__ADD_NEW_TEAMS_TO_EAST_AND_WEST_MIDLANDS_REGIONS.sql
@@ -0,0 +1,2 @@
+INSERT INTO team (code, name, ldu_code) VALUES ('N53LYA', 'Leicester Young Adults', 'N53LEIL');
+INSERT INTO team (code, name, ldu_code) VALUES ('N52SAS', 'Staffordshire IOM', 'N52STAL');
