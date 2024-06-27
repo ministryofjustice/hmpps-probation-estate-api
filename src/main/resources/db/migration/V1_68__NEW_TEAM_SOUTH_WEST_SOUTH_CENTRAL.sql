a0ea00a4a9c53fb06a54830069e431a9093b2434
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-06-27 11:30:30 +0100

    WFP-2597-new-teams (#223)

---
+++ src/main/resources/db/migration/V1_68__NEW_TEAM_SOUTH_WEST_SOUTH_CENTRAL.sql
@@ -0,0 +1,2 @@
+INSERT INTO team (code, name, ldu_code) VALUES ('N58ISC', 'BSG ISC – Bristol', 'N58BRL' );
+INSERT INTO team (code, name, ldu_code) VALUES ('N59BMK', 'Buckinghamshire and Milton Keynes 9', 'N59BUOX' );
