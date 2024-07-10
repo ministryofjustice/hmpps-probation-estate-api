36465b49e3f54824faebfe4bf9f98454e8c11b79
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-07-10 17:04:56 +0100

    WFP-2622 New team, LDU, PDU for OPD Ashfield

---
+++ src/main/resources/db/migration/V1_70__NEW_TEAMS_SOUTH_WEST_REGION_PDP.sql
@@ -0,0 +1,5 @@
+INSERT INTO pdu (code, name, region_code) VALUES('N58PDP', 'Personality Disorder Projects', 'N58');
+
+INSERT INTO ldu (code, name, pdu_code) VALUES ('N58PDP', 'PDP', 'N58PDP');
+
+INSERT INTO team (code, name, ldu_code) VALUES('N58OPA', 'OPD Ashfield Project', 'N58PDP');
