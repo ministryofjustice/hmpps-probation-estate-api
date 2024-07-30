3709cb5f830ba9a2e911a95b8ec6c243f401ff71
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-07-30 16:56:28 +0100

    WFP-2656 add 3 new teams to yath (#232)
    * WFP-2656 add 3 new teams to yath
    
    * WFP-2656 update instead of create the UPW team

---
+++ src/main/resources/db/migration/V1_73__ADD_NEW_TEAMS_TO_YATH_REGION.sql
@@ -0,0 +1,5 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N55KL5', 'Kirklees Sentence Management Team 5', 'N55KIRL');
+UPDATE team
+SET ldu_code = 'N55KUW'
+WHERE name = 'UPW - Kirklees';
+INSERT INTO team (code, name, ldu_code) VALUES('N55LM6', 'Leeds Sentence Management Team 7', 'N55LEEL');
