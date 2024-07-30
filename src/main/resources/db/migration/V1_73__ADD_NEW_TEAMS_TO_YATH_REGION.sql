30791b26cd010ed07549326177c0d453a00839b8
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-07-30 15:15:41 +0100

    WFP-2656 add 3 new teams to yath

---
+++ src/main/resources/db/migration/V1_73__ADD_NEW_TEAMS_TO_YATH_REGION.sql
@@ -0,0 +1,3 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N55KL5', 'Kirklees Sentence Management Team 5', 'N55KIRL');
+INSERT INTO team (code, name, ldu_code) VALUES('N55KUW', 'UPW - Kirklees', 'N55KIRL');
+INSERT INTO team (code, name, ldu_code) VALUES('N55LM6', 'Leeds Sentence Management Team 7', 'N55LEEL');
