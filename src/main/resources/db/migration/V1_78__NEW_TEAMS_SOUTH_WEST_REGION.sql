45d538d2fcdb71b401b5ed50243c1340981cdd5a
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-08-08 14:49:51 +0100

    WFP-2669 3 new teams south west region

---
+++ src/main/resources/db/migration/V1_78__NEW_TEAMS_SOUTH_WEST_REGION.sql
@@ -0,0 +1,3 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N58MWB', 'CIOS Sent Mgmt – Bodmin', 'N58CIOS');
+INSERT INTO team (code, name, ldu_code) VALUES('N58MWC', 'CIOS Sent Mgmt – Camborne', 'N58CIOS');
+INSERT INTO team (code, name, ldu_code) VALUES('N58MWT', 'CIOS Sent Mgmt – Truro', 'N58CIOS');
