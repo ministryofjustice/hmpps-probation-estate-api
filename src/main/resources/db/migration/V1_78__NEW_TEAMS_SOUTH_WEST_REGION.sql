eca785de40203d02499ecf605f7aa0bd8bc9d4f5
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-08-08 15:35:25 +0100

    WFP-2669 3 new teams south west region (#237)
    * WFP-2669 3 new teams south west region
    
    * WFP-2669 delete one team south west region

---
+++ src/main/resources/db/migration/V1_78__NEW_TEAMS_SOUTH_WEST_REGION.sql
@@ -0,0 +1,5 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N58MWB', 'CIOS Sent Mgmt – Bodmin', 'N58CIOS');
+INSERT INTO team (code, name, ldu_code) VALUES('N58MWC', 'CIOS Sent Mgmt – Camborne', 'N58CIOS');
+INSERT INTO team (code, name, ldu_code) VALUES('N58MWT', 'CIOS Sent Mgmt – Truro', 'N58CIOS');
+
+DELETE FROM team WHERE code = 'N58MWE'
