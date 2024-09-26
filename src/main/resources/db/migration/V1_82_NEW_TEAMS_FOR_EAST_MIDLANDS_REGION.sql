b05ede2c68b635724629f26b9a71d9ced18ff278
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-09-26 08:39:20 +0100

    WFP-2754 new teams for east midlands region

---
+++ src/main/resources/db/migration/V1_82_NEW_TEAMS_FOR_EAST_MIDLANDS_REGION.sql
@@ -0,0 +1,6 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N53DC5', 'Derby Community Hub', 'N53DCTY');
+INSERT INTO team (code, name, ldu_code) VALUES('N53DSH', 'Derbyshire Community Hub', 'N53DBSL');
+INSERT INTO team (code, name, ldu_code) VALUES('N53DBD', 'Leicestershire Community Hub', 'N53LCSL');
+INSERT INTO team (code, name, ldu_code) VALUES('N53LCH', 'Lincolnshire Community Hub', 'N53NOEW');
+INSERT INTO team (code, name, ldu_code) VALUES('N53NC8', 'Nottingham Community Hub', 'N53NOTL');
+INSERT INTO team (code, name, ldu_code) VALUES('N53NTH', 'Nottinghamshire Community Hub', 'N53NTSL');
