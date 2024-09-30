74fa848e2c15b32498e0fc7d0184d62b1eff56d7
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-09-30 09:30:37 +0100

    WFP-2759 new teams for London region

---
+++ src/main/resources/db/migration/V1_83__NEW_TEAMS_LONDON_REGION.sql
@@ -0,0 +1,3 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N07AAF', 'Awaiting Allocation Hammersmith and Fulham', 'LDNNHAF')
+INSERT INTO team (code, name, ldu_code) VALUES('N07KEW', 'Awaiting Allocation Kensington & Chelsea', 'LDNNKCW')
+INSERT INTO team (code, name, ldu_code) VALUES('N07WEU', 'Awaiting Allocation Westminster', 'LDNNKCW')
