d90480a7b0d66a76fd572505d1ee710a0c02d90c
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-10-15 10:33:25 +0100

    WFP-2775 New Teams (#247)

---
+++ src/main/resources/db/migration/V1_84__NEW_TEAMS_NORTH_WEST_AND_EAST_ENGLAND.sql
@@ -0,0 +1,2 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N56ERP', 'EXS-Resettlement Pod', 'N56ESSL');
+INSERT INTO team (code, name, ldu_code) VALUES('N51SL8', 'Liverpool South 8', 'N51SOLI');
