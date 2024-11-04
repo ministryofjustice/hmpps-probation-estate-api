c9e0db701c94e5acba10fd5bffe6ee7a04500409
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-11-04 13:55:41 +0000

    WFP-2883 New Teams (#250)

---
+++ src/main/resources/db/migration/V1_85__NEW_TEAMS_KSS_AND_SOUTH_CENTRAL.sql
@@ -0,0 +1,2 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N59PI6', 'Portsmouth and Isle of Wight 6', 'N59PSIL');
+INSERT INTO team (code, name, ldu_code) VALUES('N57SRE', 'SRY-Reset', 'N57SURL');
