0de7095c86b0497dfa6e334f3e77d0ce29be64f1
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-12-17 13:53:55 +0000

    WFP-2898 new teams for west midlands removing existing team

--- src/main/resources/db/migration/V1_96__NEW_TEAMS_FOR_W_MIDLANDS.sql
+++ src/main/resources/db/migration/V1_96__NEW_TEAMS_FOR_W_MIDLANDS.sql
@@ -1,2 +1,1 @@
 INSERT INTO team (code, name, ldu_code) VALUES('N52WNB', 'Women’s Team North Birmingham', 'N52BIRL');
-INSERT INTO team (code, name, ldu_code) VALUES('N52WSB', 'Women’s Team South Birmingham', 'N52BIRL');
