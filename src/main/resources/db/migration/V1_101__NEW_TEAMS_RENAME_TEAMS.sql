6eaa45770f141f46af16b3c016a4c7c76b92d0fc
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-02-07 15:21:04 +0000

    new teams and rename (#271)

---
+++ src/main/resources/db/migration/V1_101__NEW_TEAMS_RENAME_TEAMS.sql
@@ -0,0 +1,16 @@
+UPDATE team
+SET name = 'GM SM CP Bury Rochdale Oldham'
+WHERE code = 'N50CBA';
+
+UPDATE team
+SET name = 'GM SM CP Manchester Stockport Tameside'
+WHERE code = 'N50CAY';
+
+UPDATE team
+SET name = 'GM SM CP Bolton Wigan Salford Trafford'
+WHERE code = 'N50CAZ';
+
+--WFP2946
+INSERT INTO team (code, name, ldu_code) VALUES('N51KWM', 'KSH Women', 'N51KNOS');
+INSERT INTO team (code, name, ldu_code) VALUES('N51MAY', 'Wirral 7', 'N51WIRS');
+INSERT INTO team (code, name, ldu_code) VALUES('N07RW7', 'RWF 7', 'LDNNRWF');
