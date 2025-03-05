fb18ff26ad5d638cbc5f0177ef02dcc32d46a62c
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-03-05 14:36:34 +0000

    WFP-2973 new teams

---
+++ src/main/resources/db/migration/V1_104__NEW_TEAMS_WEST_MIDS_AND_NORTH_WEST.sql
@@ -0,0 +1,2 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N51LUA', 'Liverpool Women Unallocated', 'N51NLIS');
+INSERT INTO team (code, name, ldu_code) VALUES('N52PQP', 'BCCF PQiP', 'N52BIRL');
