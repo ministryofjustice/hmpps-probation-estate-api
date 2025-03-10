644ac3ba5ff468601846e0af2a23870a0ee1413a
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-03-10 13:00:15 +0000

    WFP-2978-2983 new teams

---
+++ src/main/resources/db/migration/V1_106__NEW_TEAMS_NORTH_EAST_AND_NORTH_WEST.sql
@@ -0,0 +1,3 @@
+INSERT INTO team (code, name, ldu_code) VALUES('N51NL6', 'Liverpool North 6', 'N51NLIS');
+INSERT INTO team (code, name, ldu_code) VALUES('N54SP1', 'Sunderland PQIP Team 1', 'N54SDL');
+INSERT INTO team (code, name, ldu_code) VALUES('N54SP2', 'Sunderland PQIP Team 2', 'N54SDL');
