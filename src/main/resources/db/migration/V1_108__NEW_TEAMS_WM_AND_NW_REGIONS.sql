a305b6de4a78773ab012d3e09860c36a85ede608
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-03-19 15:39:11 +0000

    wfp 2993 new teams (#284)

---
+++ src/main/resources/db/migration/V1_108__NEW_TEAMS_WM_AND_NW_REGIONS.sql
@@ -0,0 +1,3 @@
+--WFP-2993
+INSERT INTO team (code, name, ldu_code) VALUES('N52CRT', 'Coventry Reset Team', 'N52COVL');
+INSERT INTO team (code, name, ldu_code) VALUES('N51UKH', 'UPW KSH PDU', 'N51UPW');
