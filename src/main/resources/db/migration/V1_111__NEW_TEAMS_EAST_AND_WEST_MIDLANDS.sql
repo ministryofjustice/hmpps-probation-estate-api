92b03a6d9c9a0003390b3d56d43d864170deeafd
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-04-02 09:17:13 +0100

    new teams wfp-3031/3032

---
+++ src/main/resources/db/migration/V1_111__NEW_TEAMS_EAST_AND_WEST_MIDLANDS.sql
@@ -0,0 +1,6 @@
+--wfp-3031
+INSERT INTO team (code, name, ldu_code) VALUES('N52RTM', 'BNES Reset Team', 'N52NOEA');
+INSERT INTO team (code, name, ldu_code) VALUES('N52ST6', 'Stoke Team 6', 'N52STO');
+INSERT INTO team (code, name, ldu_code) VALUES('N52ST5', 'Stoke Team 5', 'N52STO');
+--wfp-3032
+INSERT INTO team (code, name, ldu_code) VALUES('N53DCW', 'Derby City Women''s', 'N53DCTY');
