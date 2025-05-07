feee75fb58a589fd7ef4b9ba1232e450c826672c
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-05-07 16:43:33 +0100

    new lau

--- src/main/resources/db/migration/V1_117__MISC_NEW_TEAMS.sql
+++ src/main/resources/db/migration/V1_117__MISC_NEW_TEAMS.sql
@@ -4,7 +4,8 @@ INSERT INTO team (code, name, ldu_code) VALUES('N03GWT', 'Final Third Team – G
 INSERT INTO team (code, name, ldu_code) VALUES('N52RHB', 'BCF Reset Hub', 'N52BIRL');
 INSERT INTO team (code, name, ldu_code) VALUES('N52WRH', 'Worcestershire Reactive Hub', 'N52WORL');
 --wfp-3102
-UPDATE team team set name = 'UPW SC - Bucks' where code = 'N59TAY';
+INSERT INTO ldu (code, name, pdu_code) VALUES('N59ACT', 'Unpaid Work', 'N59UPAC');
+UPDATE team set name = 'UPW SC - Bucks',ldu_code = 'N59ACT' where code = 'N59TAY';
 INSERT INTO team (code, name, ldu_code) VALUES('N59TOX', 'UPW SC - Oxon', 'N59ACT');
 INSERT INTO team (code, name, ldu_code) VALUES('N59WBI', 'West Berkshire Impact', 'N59WESB');
 
