d774bdb4393645d8ad6ac1032256c152f23d0d01
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-05-07 13:58:54 +0100

    misc new teams

---
+++ src/main/resources/db/migration/V1_117__MISC_NEW_TEAMS.sql
@@ -0,0 +1,5 @@
+--wfp-3101
+INSERT INTO team (code, name, ldu_code) VALUES('N56RPD', 'BDS-Resettlement POD', 'N56BEDL');
+INSERT INTO team (code, name, ldu_code) VALUES('N03GWT', 'Final Third Team – GWT', 'WPTGWT');
+INSERT INTO team (code, name, ldu_code) VALUES('N52RHB', 'BCF Reset Hub', 'N52BIRL');
+INSERT INTO team (code, name, ldu_code) VALUES('N52WRH', 'Worcestershire Reactive Hub', 'N52WORL');
