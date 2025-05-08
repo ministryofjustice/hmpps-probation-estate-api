a47030b33f30bb80f9342138e5dc96f234991fc3
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-05-08 13:08:10 +0100

    misc new teams (#301)
    * misc new teams
    
    * some more sc teams
    
    * rename one of the teams
    
    * new lau
    
    * need to use existing lau - error in the request

---
+++ src/main/resources/db/migration/V1_117__MISC_NEW_TEAMS.sql
@@ -0,0 +1,10 @@
+--wfp-3101
+INSERT INTO team (code, name, ldu_code) VALUES('N56RPD', 'BDS-Resettlement POD', 'N56BEDL');
+INSERT INTO team (code, name, ldu_code) VALUES('N03GWT', 'Final Third Team – GWT', 'WPTGWT');
+INSERT INTO team (code, name, ldu_code) VALUES('N52RHB', 'BCF Reset Hub', 'N52BIRL');
+INSERT INTO team (code, name, ldu_code) VALUES('N52WRH', 'Worcestershire Reactive Hub', 'N52WORL');
+--wfp-3102
+UPDATE team set name = 'UPW SC - Bucks' where code = 'N59TAY';
+INSERT INTO team (code, name, ldu_code) VALUES('N59TOX', 'UPW SC - Oxon', 'N59UPW');
+INSERT INTO team (code, name, ldu_code) VALUES('N59WBI', 'West Berkshire Impact', 'N59WESB');
+
