8cb01cfda85ceb7ce4caf68353b474dd420f139d
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-05-22 17:00:51 +0100

    remove teams and asd team

---
+++ src/main/resources/db/migration/V1_119__REMOVE_TEAMS_NEW_TEAM_NW_REGION.sql
@@ -0,0 +1,10 @@
+--wfp-3134
+DELETE FROM team where code = 'N51KN1';
+DELETE FROM team where code = 'N51KN2';
+DELETE FROM team where code = 'N51KN3';
+DELETE FROM team where code = 'N51KN4';
+DELETE FROM team where code = 'N51SH1';
+DELETE FROM team where code = 'N51SH2';
+
+--wfp-3137
+INSERT INTO team (code, name, ldu_code) values('N51REH', 'Carlisle 2', 'N51CARL'
