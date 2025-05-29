3af14210a7deb95ed617b565795b6bf26b879b1f
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-05-29 10:57:48 +0100

    team changes (#306)

---
+++ src/main/resources/db/migration/V1_120__TEAM_CHANGES_YATH_WMIDS_NE_REGION.sql
@@ -0,0 +1,7 @@
+--wfp-3139
+DELETE FROM TEAM WHERE code ='N55WCI';
+--wfp-3146
+INSERT INTO team (code, name, ldu_code) VALUES('N52WIM', 'Worcestershire IOM Team', 'N52WORL');
+INSERT INTO team (code, name, ldu_code) VALUES('N52WWT', 'Worcestershire Women''s Team', 'N52WORL');
+--wfp-3147
+INSERT INTO team (code, name, ldu_code) VALUES('N54CDR', 'CDD Resolution Team', 'N54NOR');
