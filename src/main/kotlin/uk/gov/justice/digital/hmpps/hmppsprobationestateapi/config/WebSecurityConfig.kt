ee4f0d50bfae0163457bc5ea19969184b74d3855
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-06-06 13:34:39 +0100

    permitting requests are per previous

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprobationestateapi/config/WebSecurityConfig.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppsprobationestateapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class WebSecurityConfig {
+  @Bean
+  fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+    http.csrf().disable()
+      .authorizeExchange().anyExchange().permitAll()
+      .and().build()
+}
