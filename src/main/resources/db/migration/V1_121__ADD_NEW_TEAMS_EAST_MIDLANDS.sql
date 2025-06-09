be06d71f3b8e6f6b5e1c83024da9b8a12a7acddb
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2025-06-09 10:29:27 +0100

    :card_file_box: wfp-3155: add new teams to east midlands region (#309)

---
+++ src/main/resources/db/migration/V1_121__ADD_NEW_TEAMS_EAST_MIDLANDS.sql
@@ -0,0 +1,4 @@
+--WFP-3155
+INSERT INTO team (code, name, ldu_code) VALUES('N53IL3', 'Derbyshire East 3', 'N53DBSL');
+INSERT INTO team (code, name, ldu_code) VALUES('N53BWO', 'Bassetlaw (Worksop)', 'N53NTSL');
+INSERT INTO team (code, name, ldu_code) VALUES('N53DBJ', 'Newark and Sherwood', 'N53NTSL');
