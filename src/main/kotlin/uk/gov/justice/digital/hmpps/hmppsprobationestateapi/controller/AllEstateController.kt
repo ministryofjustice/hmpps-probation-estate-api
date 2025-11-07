3d02fd48bda34548198c78d01969d4041c943fd9
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-11-07 13:10:18 +0000

    adding csv endpoint for download (#359)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprobationestateapi/controller/AllEstateController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprobationestateapi/controller/AllEstateController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsprobationestateapi.controller.dto.AllProbationDeliveryUnit
 import uk.gov.justice.digital.hmpps.hmppsprobationestateapi.controller.dto.AllRegionDeliveryUnit
 import uk.gov.justice.digital.hmpps.hmppsprobationestateapi.service.AllEstateService
+import kotlin.code
 
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -35,4 +36,30 @@ class AllEstateController(private val allEstateService: AllEstateService) {
   )
   @GetMapping("/all/regions")
   suspend fun getAllEstate(): Map<String, AllRegionDeliveryUnit> = allEstateService.getEntireEstate()
+
+  @Operation(summary = "Get all probation estate as CSV")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "200", description = "OK"),
+      ApiResponse(responseCode = "404", description = "Result Not Found"),
+    ],
+  )
+  @GetMapping("/all/regions/csv", produces = ["text/csv"])
+  suspend fun getAllEstateCsv(): String {
+    val estate = allEstateService.getEntireEstate()
+    val csvBuilder = StringBuilder()
+    csvBuilder.append("regionName,pduName,lduName,teamCode,teamName\n")
+    estate.forEach { (_, region) ->
+      region.pdus.forEach { (_, pdu) ->
+        pdu.ldus.forEach { (_, ldu) ->
+          ldu.teams.forEach { team ->
+            csvBuilder.append(
+              "\"${region.name}\",\"${pdu.name}\",\"${ldu.name}\",\"${team.code}\",\"${team.name}\"\n",
+            )
+          }
+        }
+      }
+    }
+    return csvBuilder.toString()
+  }
 }
